721a0cdd831df638e272d0c9632e05a0330739e3
chausleha <87465770+chausleha@users.noreply.github.com>
2022-07-30 18:05:36 +0300

    Выполнена логика и часть верстки страницы пользователя

--- src/app/components/user/user.component.ts
+++ src/app/components/user/user.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import {PageService} from "../../services/page.service";
+import {ActivatedRoute} from "@angular/router";
 
 @Component({
   selector: 'app-user',
@@ -6,10 +8,23 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./user.component.scss']
 })
 export class UserComponent implements OnInit {
-
-  constructor() { }
+  user: any = '';
+  constructor(private pageService: PageService, private _route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    const { id } = this._route?.snapshot.queryParams;
+    this.getUser(id);
+  }
+
+  getUser(id:any) {
+    this.pageService.getUsers(id)
+      .subscribe(response => {
+        this.user = response;
+      })
   }
 
 }
+
+
+
+
